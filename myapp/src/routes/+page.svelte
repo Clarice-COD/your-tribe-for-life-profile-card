ddef7895aabf4515c0e7bb74c540498b778d23d0
Clarice-COD <clariceboerlijst@gmail.com>
2025-09-09 22:14:10 +0200

    Test data inladen website
    #7

--- myapp/src/routes/+page.svelte
+++ myapp/src/routes/+page.svelte
@@ -1,2 +1,15 @@
+<script>
+    export let data;
+    const { mymembers }=data;
+
+    // Check data load
+    console.log(mymembers);
+  </script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<!-- test -->
+ {#each mymembers as mymember}
+    <p>{mymember.name}</p>
+ {/each}
